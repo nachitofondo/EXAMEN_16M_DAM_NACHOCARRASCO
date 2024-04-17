808ad816f6ac0b54bc8fe944da42fdac81edbfe1
ignac <ignac@equipo_de_nacho>
2024-04-17 12:22:31 +0200

    segunda edicion

--- src/prueba/prueba.java
+++ src/prueba/prueba.java
@@ -3,7 +3,9 @@ package prueba;
 public class prueba {
 	public static void main(String[] arg) {
 		System.out.println("Hola mundo");
-		System.out.println(("Adiós mundo"));
+		System.out.println("Adiós mundo");
+		System.out.println("Amai la kuki");
+		
 	}
 
  }
