11b157fa92bbccd47d90c3204a312ab9d70be0b0
ignac <ignac@equipo_de_nacho>
2024-04-24 12:16:52 +0200

    prueba

--- src/prueba/prueba.java
+++ src/prueba/prueba.java
@@ -2,10 +2,8 @@ package prueba;
 
 public class prueba {
 	public static void main(String[] arg) {
-		System.out.println("Hola mundo");
-		System.out.println("Adiós mundo");
-		System.out.println("Amai la kuki");
-		System.out.println("holaaaaç");
+		System.out.println("Hola soy nacho");
+		
 		
 	}
 
