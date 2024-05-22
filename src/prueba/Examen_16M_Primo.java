b6d16c9ab44aa00fb4b9c36273025348896b58d7
ignac <ignac@equipo_de_nacho>
2024-05-22 12:18:27 +0200

    hola

---
+++ src/prueba/Examen_16M_Primo.java
@@ -0,0 +1,12 @@
+package prueba;
+
+public class Examen_16M_Primo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Buenos dias Ceinmark,soy Nacho Carrasco");
+			System.out.println("Introduce un numero para averiguar si es primo o no ");
+
+	}
+
+}
